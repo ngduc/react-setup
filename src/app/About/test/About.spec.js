2b74c4b25acf134231f01b01c64e463e83a7e2ab
Duc Ng <ducjava@gmail.com>
2016-01-22 12:24:39 -0800

    chore: added About.spec.js

---
+++ src/app/About/test/About.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import jsdom from 'mocha-jsdom';
+import { TestUtils, createRenderer } from 'react-addons-test-utils';
+import $ from 'teaspoon';
+
+import { IntlProvider } from 'react-intl';
+
+import About from '../About';
+
+describe('About', () => {
+  jsdom();
+  const renderer = createRenderer();
+  const intlProvider = new IntlProvider({ locale: 'en' }, {});
+  let $el;
+
+  const reactEl = (<IntlProvider><About/></IntlProvider>);
+
+  beforeEach(() => {
+    const { intl } = intlProvider.getChildContext();
+    renderer.render(reactEl, { intl });
+    $el = $(renderer.getRenderOutput()).render({ intl });
+  });
+
+  it('should contain "About"', () => {
+    const html = $el.find('section')[0].innerHTML;
+    expect(html.indexOf('About')).to.be.above(0);
+  });
+});

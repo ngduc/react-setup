0ccfdbf6f7c560b6f3e85cc4d10bedf923400b1b
Duc Ng <ducjava@gmail.com>
2016-02-07 08:39:27 -0800

    chore: upgraded dependencies to use koa 2

--- devServer.js
+++ devServer.js
@@ -1,14 +1,15 @@
-const koa = require('koa');
+const Koa = require('koa')
+const convert = require('koa-convert')
+
 const webpackDevServer = require('koa-webpack-dev');
 const configFilePath = './webpack.config.dev';
 const config = require(configFilePath);
 
-const app = koa();
+const app = new Koa();
 
-app.use(webpackDevServer({
+app.use(convert(webpackDevServer({
     config: configFilePath
-}));
+})));
 
 console.info(`🌎  Listening on port ${config._hotPort}. Open up http://localhost:${config._hotPort}/ in your browser.`);
-
 app.listen(8000);

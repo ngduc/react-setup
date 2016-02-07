1963a4849161041021509f0c0a63d3c2d30fb48f
Duc Ng <ducjava@gmail.com>
2016-02-06 22:31:04 -0800

    feat: added koa2 dependencies; updated devServer.js to use koa2;

--- devServer.js
+++ devServer.js
@@ -1,32 +1,14 @@
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config.dev');
+const koa = require('koa');
+const webpackDevServer = require('koa-webpack-dev');
+const configFilePath = './webpack.config.dev';
+const config = require(configFilePath);
 
-const app = express();
+const app = koa();
 
-const compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath,
-  contentBase: 'src',
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false
-  }
+app.use(webpackDevServer({
+    config: configFilePath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+console.info(`🌎  Listening on port ${config._hotPort}. Open up http://localhost:${config._hotPort}/ in your browser.`);
 
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(config._hotPort, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.info(`🌎  Listening on port ${config._hotPort}. Open up http://localhost:${config._hotPort}/ in your browser.`);
-});
+app.listen(8000);

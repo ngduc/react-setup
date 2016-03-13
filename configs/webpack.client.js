3c6a1de9318aa2217411d1ec7b7ee1f855aeff63
Duc Ng <ducjava@gmail.com>
2016-03-13 16:34:33 -0700

    fix: npm run dev

--- configs/webpack.client.js
+++ configs/webpack.client.js
@@ -21,7 +21,6 @@ module.exports = {
       to: '../static'   // copy to dist/views
     }]),
 		new webpack.DefinePlugin({__CLIENT__: true, __SERVER__: false, __PRODUCTION__: true, __DEV__: false}),
-		new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
